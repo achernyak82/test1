b6c45792bfa10f1691dd948780e51e9bf6e3629b
Alexander Chernyak <achernyak82@mail.ru>
2017-12-09 10:20:21 +0200

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'app';
+  public path = '';
+
+  constructor() { }
+
+  ngOnInit() {
+    this.path = 'T'; // значение по умолчанию в сетапе
+  }
+
+  onclick1() {
+   this.path = 'T';
+  }
+
+  onclick2() {
+    this.path = 'I';
+  }
+
+  onclick3() {
+    this.path = 'A';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { Card1Component } from './card1/card1.component';
+import { Card2Component } from './card2/card2.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    Card1Component,
+    Card2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
